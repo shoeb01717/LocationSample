460fac3aed7e122410c265358f4e8cb2fd599acf
Shoeb Amin <ShoebAmin@Ss-MacBook-Pro.local>
2016-09-05 22:53:16 +0900

    initial project import gitignore update

---
+++ LocationSample/ViewControllers/CurrentLocationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CurrentLocationViewController.swift
+//  LocationSample
+//
+//  Created by Shoeb Amin on 9/5/16.
+//  Copyright © 2016 SiliconOrchard. All rights reserved.
+//
+
+import UIKit
+
+class CurrentLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
